b6fcbad0d91ed9dfd5b15d94ce70ef59a83602e0
AlanMandriaza <alan.mandriaza@gmail.com>
2023-03-09 21:34:51 +0000

    repodruce

--- src/js/AudioPlayer.jsx
+++ src/js/AudioPlayer.jsx
@@ -1,11 +1,7 @@
-import React, { useState, useRef } from 'react';
-import TimeSeeker from './TimeSeeker.jsx';
-
+import React, { useState, useRef } from "react";
 function AudioPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [currentTime, setCurrentTime] = useState(0);
   const audioRef = useRef(null);
-
   function handlePlayPause() {
     if (isPlaying) {
       audioRef.current.pause();
@@ -14,20 +10,16 @@ function AudioPlayer() {
     }
     setIsPlaying(!isPlaying);
   }
-  function handleTimeUpdate() {
-    setCurrentTime(audioRef.current.currentTime);
-  }
-
   return (
     <div>
       <audio
         ref={audioRef}
         src="https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3"
-        onTimeUpdate={handleTimeUpdate}
         onEnded={() => setIsPlaying(false)}
       />
-      <button onClick={handlePlayPause}>{isPlaying ? 'Pausar' : 'Reproducir'}</button>
-      <TimeSeeker currentTime={currentTime} audioRef={audioRef} />
+      <button onClick={handlePlayPause}>
+        {isPlaying ? "Pausar" : "Reproducir"}
+      </button>
     </div>
   );
 }
--- src/js/component/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AudioPlayer from './AudioPlayer.jsx';
-
-ReactDOM.render(<AudioPlayer />, document.querySelector('#app'));
