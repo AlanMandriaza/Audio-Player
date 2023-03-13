09baa9682cd60d7cbf54de4e4140564edb7ce9f3
AlanMandriaza <alan.mandriaza@gmail.com>
2023-03-13 14:35:46 +0000

    reparando bug

--- src/js/AudioPlayer.jsx
+++ src/js/AudioPlayer.jsx
@@ -1,58 +1,63 @@
-import React from 'react';
-
-import audioFiles from './audioFiles';
-
-// Define el estado inicial
-const initialState = {
-  selectedSong: null,
-  audio: null
-};
-
-// Define el reducer que actualiza el estado
-function reducer(state, action) {
-  switch (action.type) {
-    case 'SELECT_SONG':
-      return { ...state, selectedSong: action.payload };
-    case 'SET_AUDIO':
-      return { ...state, audio: action.payload };
-    default:
-      return state;
-  }
-}
+import React, { useState, useRef, useEffect } from "react";
 
 function AudioPlayer() {
-  
-  // Inicializa el estado usando el reducer
-  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songs, setSongs] = useState([]);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const audioRef = useRef(null);
 
-  // Define la función para manejar la selección de una canción
-  function handleSelectSong(song) {
-    // Crea una instancia del audio para la canción seleccionada
-    const audio = new Audio(`/songs/${song.file}`);
-    // Actualiza el estado con la canción seleccionada y su instancia de audio correspondiente
-    dispatch({ type: 'SELECT_SONG', payload: song });
-    dispatch({ type: 'SET_AUDIO', payload: audio });
-  }
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/sound/songs")
+      .then((response) => response.json())
+      .then((data) => setSongs(data));
+  }, []);
 
-  // Define la función para manejar el play/pause del audio
   function handlePlayPause() {
-    const audio = state.audio;
-    if (audio.paused) {
-      audio.play();
+    if (isPlaying) {
+      audioRef.current.pause();
     } else {
-      audio.pause();
+      audioRef.current.play();
     }
+    setIsPlaying(!isPlaying);
   }
 
-  // Renderiza la vista previa del reproductor de audio
+  function handleNextSong() {
+    setCurrentSongIndex((currentSongIndex + 1) % songs.length);
+    setIsPlaying(false);
+  }
+
+  function handlePrevSong() {
+    setCurrentSongIndex(
+      currentSongIndex === 0 ? songs.length - 1 : currentSongIndex - 1
+    );
+    setIsPlaying(false);
+  }
+
+  function handleSelectSong(song) {
+    const songURL = "https://assets.breatheco.de/apis/sound/" + song.url;
+    const index = songs.findIndex((s) => s.name === song.name);
+    setCurrentSongIndex(index);
+    audioRef.current.src = songURL;
+    audioRef.current.play().catch((error) => console.error(error));
+    setIsPlaying(true);
+  }
+
   return (
     <div>
-      <h2>{state.selectedSong ? state.selectedSong.name : 'Selecciona una canción'}</h2>
-      <button onClick={handlePlayPause}>{state.audio && state.audio.paused ? 'Play' : 'Pause'}</button>
+      <audio
+        ref={audioRef}
+        src={`https://assets.breatheco.de/apis/sound/${songs[currentSongIndex]?.url}`}
+        onEnded={handleNextSong}
+      />
+      <button onClick={handlePrevSong}>Previous</button>
+      <button onClick={handlePlayPause}>{isPlaying ? "Pause" : "Play"}</button>
+      <button onClick={handleNextSong}>Next</button>
       <ul>
-        {/* Recorre el arreglo de canciones para renderizar una lista de botones que permiten seleccionar una canción */}
-        {audioFiles.map(song => (
-          <li key={song.name} onClick={() => handleSelectSong(song)}>
+        {songs.map((song) => (
+          <li
+            key={`${song.id}-${song.name}`}
+            onClick={() => handleSelectSong(song)}
+          >
             {song.name}
           </li>
         ))}
@@ -61,5 +66,4 @@ function AudioPlayer() {
   );
 }
 
-// Exporta el componente por defecto del archivo
 export default AudioPlayer;
