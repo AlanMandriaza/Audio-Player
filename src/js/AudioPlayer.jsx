ad40f308156d76e3a2a98d0ff18c2bc19c1ffffd
AlanMandriaza <alan.mandriaza@gmail.com>
2023-03-13 18:07:56 +0000

    terminado

--- src/js/AudioPlayer.jsx
+++ src/js/AudioPlayer.jsx
@@ -6,7 +6,10 @@ function AudioPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [songs, setSongs] = useState([]);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0); // Estado para el tiempo actual de reproducción
+  const [duration, setDuration] = useState(0); // Estado para la duración total de la canción
   const audioRef = useRef(null);
+  const progressBarRef = useRef(null);
 
   useEffect(() => {
     fetch("https://assets.breatheco.de/apis/sound/songs")
@@ -44,15 +47,36 @@ function AudioPlayer() {
     setIsPlaying(true);
   }
 
+  function handleTimeUpdate() {
+    setCurrentTime(audioRef.current.currentTime);
+    setDuration(audioRef.current.duration);
+  }
+
+  function handleProgressBarClick(event) {
+    const newTime =
+      (event.offsetX / progressBarRef.current.offsetWidth) * duration;
+    audioRef.current.currentTime = newTime;
+    setCurrentTime(newTime);
+  }
+  function formatTime(timeInSeconds) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60)
+      .toString()
+      .padStart(2, "0");
+    return `${minutes}:${seconds}`;
+  }
+
   return (
     <div className="audio-player">
       <audio
         ref={audioRef}
         src={`https://assets.breatheco.de/apis/sound/${songs[currentSongIndex]?.url}`}
         onEnded={handleNextSong}
+        onTimeUpdate={handleTimeUpdate}
       />
 
       <ol>
+        {/* Lista de canciones */}
         {songs.map((song, index) => (
           <li
             key={`${song.id}-${song.name}`}
@@ -60,25 +84,36 @@ function AudioPlayer() {
             className={index === currentSongIndex ? "selected" : ""}
           >
             {song.name}
-            
           </li>
         ))}
 
+        {/* Botones para controlar la reproducción */}
         <div className="botones d-flex">
-        <button onClick={handlePrevSong}>
-          <FaArrowLeft />
-        </button>
-        <button onClick={handlePlayPause}>
-          {isPlaying ? <FaPause /> : <FaPlay />}
-        </button>
-
-        <button onClick={handleNextSong}>
-          <FaArrowRight />
-        </button>
+          <button onClick={handlePrevSong}>
+            <FaArrowLeft />
+          </button>
+          <button onClick={handlePlayPause}>
+            {isPlaying ? <FaPause /> : <FaPlay />}
+          </button>
+          <button onClick={handleNextSong}>
+            <FaArrowRight />
+          </button>
+          {/* Barra de progreso */}
+          <div className="progress-bar-container">
+            <progress
+              ref={progressBarRef}
+              value={currentTime}
+              max={duration}
+              onClick={handleProgressBarClick}
+            />
+            <div className="current-time">
+              {isNaN(currentTime) ? "0:00" : formatTime(currentTime)}{" "}
+              {isNaN(duration) ? "0:00" : formatTime(duration)}
+            </div>
+          </div>
         </div>
       </ol>
     </div>
   );
 }
-
 export default AudioPlayer;
